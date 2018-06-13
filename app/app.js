69aa66c7fe92ddaa821f44fcfa49fa87df7cc1a4
funnythingz <funnythingz69+github@gmail.com>
2018-06-13 21:31:10 +0900

    buildできるように修正

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
-import Vue from 'vue';
-import App from './zipcode_search.vue';
-var app = new Vue({
-  el: '#app',
-  components: zipcode_search
+import Vue from 'vue'
+import ZipcodeSearch from './zipcode_search.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const App = new Vue({
+        el: '#app',
+        render: h => h(ZipcodeSearch)
+    })
 })
--- app/zipcode_search.vue
+++ app/zipcode_search.vue
@@ -3,37 +3,37 @@
     <h1>sample</h1>
   </div>
   <!-- div.zipcode
-    input(type="text" v-model="zipcode")
-    button(@click='search')
-      | 検索
-    p {{zipcode}}の住所は
-    p {{addre1}}
-    p {{addre2}}
-    p {{addre3}} -->
+  input(type="text" v-model="zipcode")
+  button(@click='search')
+  | 検索
+  p {{zipcode}}の住所は
+  p {{addre1}}
+  p {{addre2}}
+  p {{addre3}} -->
 </template>
 <script>
 export default{
-//   data(){
-//     return {
-//       zipcode: '',
-//       addre1: '',
-//       addre2: '',
-//       addre3: ''
-//     }
-//   },
-//   methods:{
-//     search: function(){
-//       this.$jsonp('http://zipcloud.ibsnet.co.jp/api/search', {zipcode: this.zipcode})
-//       .then(res => {
-//         // Success.
-//         this.addre1 = res.results[0].address1
-//         this.addre2 = res.results[0].address2
-//         this.addre3 = res.results[0].address3
-//       }).catch(err => {
-//         // Failed.
-//       })
-//     }
-//   }
+  //   data(){
+  //     return {
+  //       zipcode: '',
+  //       addre1: '',
+  //       addre2: '',
+  //       addre3: ''
+  //     }
+  //   },
+  //   methods:{
+  //     search: function(){
+  //       this.$jsonp('http://zipcloud.ibsnet.co.jp/api/search', {zipcode: this.zipcode})
+  //       .then(res => {
+  //         // Success.
+  //         this.addre1 = res.results[0].address1
+  //         this.addre2 = res.results[0].address2
+  //         this.addre3 = res.results[0].address3
+  //       }).catch(err => {
+  //         // Failed.
+  //       })
+  //     }
+  //   }
 }
 </script>
 <style>
